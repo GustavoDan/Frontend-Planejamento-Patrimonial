98a1b5daafe9c75e0116c487ea2173fb9df3109a
Gustavo Daniel <gudaniel1@gmail.com>
2025-08-10 21:54:10 -0300

    Create svg icon components

---
+++ src/components/icons/ChevronDownIcon.tsx
@@ -0,0 +1,17 @@
+export const ChevronDownIcon = () => (
+    <svg
+        width="20"
+        height="12"
+        viewBox="0 0 20 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M2 2L10 10L18 2"
+            stroke="#9F9F9F"
+            stroke-width="4"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </svg>
+);
---
+++ src/components/icons/ChevronUpIcon.tsx
@@ -0,0 +1,17 @@
+export const ChevronUpIcon = () => (
+    <svg
+        width="20"
+        height="12"
+        viewBox="0 0 20 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M18 10L10 2L2 10"
+            stroke="#9F9F9F"
+            stroke-width="4"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </svg>
+);
