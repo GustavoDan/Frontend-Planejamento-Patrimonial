d98d4e5b964a70a42e4eff4b5d7ee1432afb37f5
Gustavo Daniel <gudaniel1@gmail.com>
2025-08-13 12:37:50 -0300

    Add variation to DisplayClientMoney

--- src/components/dashboard/DisplayClientMoney.tsx
+++ src/components/dashboard/DisplayClientMoney.tsx
@@ -10,10 +10,11 @@ const DisplayClientMoney = ({ walletBalance }: DisplayClientMoneyProps) => {
     return (
         <div className="flex flex-col font-workSans pt-5 pl-5">
             <span className="text-md font-inter">Total Alocado</span>
-            <span className="text-2xl font-semibold text-[#757575]">
+            <span className="text-4xl font-semibold text-[#757575]">
                 {balanceParts.currency}{" "}
                 <span className="text-white">{balanceParts.value}</span>
                 {balanceParts.cents}
+                <span className="text-[#68AAF1] text-lg">+12,37%</span>
             </span>
         </div>
     );
