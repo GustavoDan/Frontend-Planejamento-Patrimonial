3ea75a051e51697ff75c869e33367cd8da8a1b55
Gustavo Daniel <gudaniel1@gmail.com>
2025-08-13 20:10:36 -0300

    Add redirect to projection page from ViewSimulationButton

--- src/components/history/ViewSimulationButton.tsx
+++ src/components/history/ViewSimulationButton.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
 
+const mockClientId = "1";
+const mockSimulationId = "1";
+
 const ViewSimulationButton = () => {
+    const router = useRouter();
     return (
-        <Button className="text-[#B4B4B4] bg-[#363636] rounded-[2.5rem] hover:bg-zinc-800 hover:text-zinc-400">
+        <Button
+            className="text-[#B4B4B4] bg-[#363636] rounded-[2.5rem] hover:bg-zinc-800 hover:text-zinc-400"
+            onPointerUp={() =>
+                router.push(
+                    `/projection?clientId=${mockClientId}&simulationId=${mockSimulationId}`
+                )
+            }
+        >
             Ver no gráfico
         </Button>
     );
